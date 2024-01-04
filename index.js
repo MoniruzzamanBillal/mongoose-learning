f74ca6492ff693cb04f0855a6845972774a38d01
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-04 11:48:53 +0600

    connecting to mmongoose server done

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require("mongoose");
 const app = express();
 
 const port = process.env.port || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+// connecting to mongoose
+mongoose
+  .connect("mongodb://127.0.0.1:27017/todos")
+  .then(() => console.log("connected to mongodb"))
+  .catch((error) => console.log(error));
 
 app.get("/", async (req, res) => {
   res.send({ message: "mongoose server is running " });
